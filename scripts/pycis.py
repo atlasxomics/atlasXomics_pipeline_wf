c2049bf6ba7e2b57aa0a26bf2626acb0b64bdf43
jpmcga <jpaulmcgann@gmail.com>
2024-06-05 16:07:55 -0400

    fix: clean pycistopic code

--- scripts/pycis.py
+++ scripts/pycis.py
@@ -1,18 +1,22 @@
 import argparse
+import glob
 import gzip
 import os
 import pandas as pd
-import pathlib
 import pickle
 import pyranges as pr
 import pybiomart as pbm
 
-from pycisTopic.qc import *
-from pycisTopic.cistopic_class import *
-from pycisTopic.pseudobulk_peak_calling import *
-from pycisTopic.iterative_peak_calling import *
+from pathlib import Path
 
+from pycisTopic.cistopic_class import create_cistopic_object_from_fragments
+from pycisTopic.pseudobulk_peak_calling import export_pseudobulk, peak_calling
+from pycisTopic.qc import (
+    compute_qc_stats, plot_sample_metrics, plot_barcode_metrics
+)
+from pycisTopic.iterative_peak_calling import get_consensus_peaks
 
+# globals
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", required=True)
 ap.add_argument("-i", required=True)
@@ -20,8 +24,6 @@ ap.add_argument("-g", required=True)
 ap.add_argument("-c", required=True)
 ap.add_argument("-k", required=True)
 ap.add_argument("-w", required=True)
-ap.add_argument("-d", required=True)
-ap.add_argument("-t", required=True)
 ap.add_argument("-p", required=True)
 
 args = vars(ap.parse_args())
@@ -32,11 +34,12 @@ genome = args["g"]
 chrsize = args["c"]
 black_lst = args["k"] if genome != "rnor6" else None
 whitelist = args["w"]
-work_dir = args["d"]
-tmp_dir = args["t"]
 positions_file = args["p"]
 
-#########################################
+work_dir = Path("Statistics").resolve()
+tmp_dir = Path("tmp_dir").resolve()
+
+# Filter fragment file, save path in dict for pyscistopic
 print("preparing fragment file starting")
 
 with gzip.open(fragment, "rb") as file:
@@ -54,12 +57,11 @@ fragment_edited = f"{work_dir}/fragments_edited.tsv.gz"
 subset_df.to_csv(
    fragment_edited, index=False, compression="gzip", sep="\t", header=None
 )
-
-#################################pycisTopic###############################
-print("pycisTopic Starting ...")
-
 fragments_dict = {run_id: fragment_edited}
 
+print("pycisTopic Starting ...")
+
+# Prepare cell_data, chomsizes for pycistopic
 cell_data = pd.read_table(whitelist, header=None, index_col=None)
 cell_data.rename(columns={0: "barcode"}, inplace=True)
 cell_data.index = (
@@ -75,32 +77,24 @@ chromsizes.columns = ["Chromosome", "End"]
 chromsizes["Start"] = [0] * chromsizes.shape[0]
 chromsizes = chromsizes.loc[:, ["Chromosome", "Start", "End"]]
 
-# Exceptionally in this case, to agree with CellRangerARC annotations
-chromsizes["Chromosome"] = [
-    chromsizes["Chromosome"][x].replace("v", ".")
-    for x in range(len(chromsizes["Chromosome"]))
-]
-chromsizes["Chromosome"] = [
-    chromsizes["Chromosome"][x].split("_")[1]
-    if len(chromsizes["Chromosome"][x].split("_")) > 1
-    else chromsizes["Chromosome"][x]
-    for x in range(len(chromsizes["Chromosome"]))
-]
+# make format agree with CellRangerARC annotations
+chromsizes["Chromosome"] = chromsizes["Chromosome"].str.replace("v", ".")
+chromsizes["Chromosome"] = chromsizes["Chromosome"].apply(
+    lambda x: x.split("_")[1] if len(x.split("_")) > 1 else x
+)
 chromsizes = pr.PyRanges(chromsizes)
 
+# Run pseudobulk
 bw_paths, bed_paths = export_pseudobulk(
     input_data=cell_data,
     variable="variable",
     sample_id_col="Sample",
     chromsizes=chromsizes,
-    bed_path=work_dir + "/scATAC/consensus_peak_calling/pseudobulk_bed_files/",
-    bigwig_path=work_dir + "/scATAC/consensus_peak_calling/pseudobulk_bw_files/",
+    bed_path=f"{work_dir}/scATAC/consensus_peak_calling/pseudobulk_bed_files/",
+    bigwig_path=f"{work_dir}/scATAC/consensus_peak_calling/pseudobulk_bw_files/",
     path_to_fragments=fragments_dict,
     n_cpu=77,  # sometimes increasing n_cpu > 1 results in  ray memory error
-    normalize_bigwig=True,
-    remove_duplicates=True,
-    _temp_dir=tmp_dir + "ray_spill",  # tmp dir must not be too long
-    split_pattern="___",
+    _temp_dir=f"{tmp_dir}/ray_spill",
     use_polars=False
 )
 
@@ -116,50 +110,34 @@ with open(
 ) as f:
     pickle.dump(bw_paths, f)
 
-infile = open(
-   f"{work_dir}/scATAC/consensus_peak_calling/pseudobulk_bed_files/bed_paths.pkl",
-   "rb"
-)
-bed_paths = pickle.load(infile)
-infile.close()
-
-macs_path = "/usr/local/bin/macs2"
-outdir = work_dir + "/scATAC/consensus_peak_calling/MACS/"
-
-genome_size = genome if genome != "rnor6" else "2.9e9"
-
-# Run peak calling
+# Run peak calling, derive consensus peaks, write to .bed
 narrow_peaks_dict = peak_calling(
-    macs_path,
+    "/usr/local/bin/macs2",
     bed_paths,
-    outdir,
-    genome_size=genome_size,
+    f"{work_dir}/scATAC/consensus_peak_calling/MACS/",
+    genome_size=genome if genome != "rnor6" else "2.9e9",
     n_cpu=77,
-    input_format="BEDPE",
-    shift=73,
-    ext_size=146,
-    keep_dup="all",
-    q_value=0.05,
-    _temp_dir=tmp_dir + "ray_spill"
+    _temp_dir=f"{tmp_dir}/ray_spill"
 )
 
-peak_half_width = 250
-consensus_peaks = get_consensus_peaks(  # iterative_peak_calling
+consensus_peaks = get_consensus_peaks(
    narrow_peaks_dict,
-   peak_half_width,
+   peak_half_width=250,
    chromsizes=chromsizes,
    path_to_blacklist=black_lst
 )
 
-# Write to bed
 consensus_peaks.to_bed(
-   path=work_dir + "/scATAC/consensus_peak_calling/consensus_regions.bed",
+   path=f"{work_dir}/scATAC/consensus_peak_calling/consensus_regions.bed",
    keep=True,
    compression="infer",
    chain=False
 )
 
+# Quality Control
 print("Quality control started ...")
+
+# Create annot for tss calculation
 if genome == "mm":
     dataset = pbm.Dataset(
        name="mmusculus_gene_ensembl",
@@ -199,6 +177,7 @@ annot["Chromosome/scaffold name"] = annot[
 annot.columns = ["Chromosome", "Start", "Strand", "Gene", "Transcript_type"]
 annot = annot[annot.Transcript_type == "protein_coding"]
 
+# Compute qc stats, save as .pkl
 path_to_regions = {
    run_id: f"{work_dir}/scATAC/consensus_peak_calling/consensus_regions.bed"
 }
@@ -220,42 +199,31 @@ metadata_bc, profile_data_dict = compute_qc_stats(
     n_frag=100,
     n_bc=None,
     tss_flank_window=1000,
-    tss_window=50,
     tss_minimum_signal_window=100,
     tss_rolling_window=10,
     remove_duplicates=True,
-    _temp_dir=os.path.join(tmp_dir + 'ray_spill'),
+    _temp_dir=f"{tmp_dir}/ray_spill",
     use_polars=False  # True gives TypeError: __init__() got an unexpected
 )                     # keyword argument 'encoding'.
 
-if not os.path.exists(f"{work_dir}/scATAC/quality_control"):
-    os.makedirs(f"{work_dir}/scATAC/quality_control")
-
-pickle.dump(
-    metadata_bc,
-    open(f"{work_dir}/scATAC/quality_control/metadata_bc.pkl", "wb")
-)
-
-pickle.dump(
-    profile_data_dict,
-    open(f"{work_dir}/scATAC/quality_control/profile_data_dict.pkl", "wb")
-)
+os.makedirs(f"{work_dir}/scATAC/quality_control", exist_ok=True)
 
 with open(f"{work_dir}/scATAC/quality_control/metadata_bc.pkl", "wb") as f:
     pickle.dump(metadata_bc, f)
 
-with open(
-    f"{work_dir}/scATAC/quality_control/profile_data_dict.pkl", "wb"
-) as f:
+with open(f"{work_dir}/scATAC/quality_control/profile_data_dict.pkl", "wb") as f:
     pickle.dump(profile_data_dict, f)
 
+# Create standard QC plot
 plot_sample_metrics(
     profile_data_dict,
     insert_size_distribution_xlim=[0, 600],
     ncol=5,
     plot=True,
-    save=f"{work_dir}/scATAC/quality_control/sample_metrics.pdf")
+    save=f"{work_dir}/scATAC/quality_control/sample_metrics.pdf"
+)
 
+# Calculate number of barcodes passing filter and save as .pkl
 qc_filters = {
     run_id: {
         "Log_unique_nr_frag": [0, None],
@@ -265,139 +233,70 @@ qc_filters = {
     }
 }
 
-# Return figure to plot together with other metrics, and cells passing filters.
-# Figure will be saved as pdf.
-FRIP_NR_FRAG_filterDict = {}
-TSS_NR_FRAG_filterDict = {}
-FRIP_NR_FRAG_figDict = {}
-TSS_NR_FRAG_figDict = {}
-DR_NR_FRAG_figDict = {}
+FRIP_filterDict = {}
+TSS_filterDict = {}
 
-for runID in metadata_bc:
-    FRIP_NR_FRAG_fig, FRIP_NR_FRAG_filter = plot_barcode_metrics(
-        metadata_bc[runID],
-        var_x="Log_unique_nr_frag",
-        var_y="FRIP",
-        min_x=qc_filters[runID]["Log_unique_nr_frag"][0],
-        max_x=qc_filters[runID]["Log_unique_nr_frag"][1],
-        min_y=qc_filters[runID]["FRIP"][0],
-        max_y=qc_filters[runID]["FRIP"][1],
-        return_cells=True,
-        return_fig=True,
-        plot=False
-    )
+_, FRIP_filter = plot_barcode_metrics(
+    metadata_bc[run_id],
+    var_x="Log_unique_nr_frag",
+    var_y="FRIP",
+    min_x=qc_filters[run_id]["Log_unique_nr_frag"][0],
+    max_x=qc_filters[run_id]["Log_unique_nr_frag"][1],
+    min_y=qc_filters[run_id]["FRIP"][0],
+    max_y=qc_filters[run_id]["FRIP"][1],
+    return_cells=True,
+    return_fig=True,
+    plot=False
+)
 
-    # Return figure plot together with other metrics, cells passing filters.
-    TSS_NR_FRAG_fig, TSS_NR_FRAG_filter = plot_barcode_metrics(
-        metadata_bc[runID],
-        var_x='Log_unique_nr_frag',
-        var_y='TSS_enrichment',
-        min_x=qc_filters[runID]['Log_unique_nr_frag'][0],
-        max_x=qc_filters[runID]['Log_unique_nr_frag'][1],
-        min_y=qc_filters[runID]['TSS_enrichment'][0],
-        max_y=qc_filters[runID]['TSS_enrichment'][1],
-        return_cells=True,
-        return_fig=True,
-        plot=False
-    )
+_, TSS_filter = plot_barcode_metrics(
+    metadata_bc[run_id],
+    var_x='Log_unique_nr_frag',
+    var_y='TSS_enrichment',
+    min_x=qc_filters[run_id]['Log_unique_nr_frag'][0],
+    max_x=qc_filters[run_id]['Log_unique_nr_frag'][1],
+    min_y=qc_filters[run_id]['TSS_enrichment'][0],
+    max_y=qc_filters[run_id]['TSS_enrichment'][1],
+    return_cells=True,
+    return_fig=True,
+    plot=False
+)
 
-    # Return figure to plot together with other metrics, but not returning
-    # cells (no filter applied for the duplication rate  per barcode)
-    DR_NR_FRAG_fig = plot_barcode_metrics(
-        metadata_bc[runID],
-        var_x="Log_unique_nr_frag",
-        var_y="Dupl_rate",
-        min_x=qc_filters[runID]["Log_unique_nr_frag"][0],
-        max_x=qc_filters[runID]["Log_unique_nr_frag"][1],
-        min_y=qc_filters[runID]["Dupl_rate"][0],
-        max_y=qc_filters[runID]["Dupl_rate"][1],
-        return_cells=False,
-        return_fig=True,
-        plot=False,
-        plot_as_hexbin=True
-    )
+FRIP_filterDict[run_id] = FRIP_filter
+TSS_filterDict[run_id] = TSS_filter
 
-    # Barcodes passing filters
-    FRIP_NR_FRAG_filterDict[runID] = FRIP_NR_FRAG_filter
-    TSS_NR_FRAG_filterDict[runID] = TSS_NR_FRAG_filter
-
-    # Figs
-    FRIP_NR_FRAG_figDict[runID] = FRIP_NR_FRAG_fig
-    TSS_NR_FRAG_figDict[runID] = TSS_NR_FRAG_fig
-    DR_NR_FRAG_figDict[runID] = DR_NR_FRAG_fig
-
-bc_passing_filters = dict()
-for runID in metadata_bc:
-    bc_passing_filters[runID] = list(
-        (set(FRIP_NR_FRAG_filterDict[runID]) &
-         set(TSS_NR_FRAG_filterDict[runID]))
-    )
-    bc_len = len(bc_passing_filters[runID])
-    print(
-        f"{bc_len} barcodes passed filters for sample {runID}"
-    )
+bc_passing_filters = {
+    run_id: list(set(FRIP_filterDict[run_id]) & set(TSS_filterDict[run_id]))
+}
+bc_len = len(bc_passing_filters[run_id])
+print(f"{bc_len} barcodes passed filters for sample {run_id}")
 
 with open(
     f"{work_dir}/scATAC/quality_control/bc_passing_filters.pkl", "wb"
 ) as f:
     pickle.dump(bc_passing_filters, f)
 
-metadata_bc = pickle.load(
-    open(
-        os.path.join(f"{work_dir}/scATAC/quality_control/metadata_bc.pkl"),
-        "rb"
-    )
+# Create cistopic object, save metrics to cistopic_cell_data.csv
+cistopic_obj = create_cistopic_object_from_fragments(
+    path_to_fragments=fragments_dict[run_id],
+    path_to_regions=path_to_regions[run_id],
+    path_to_blacklist=black_lst,
+    metrics=metadata_bc[run_id],
+    valid_bc=bc_passing_filters[run_id],
+    n_cpu=77,
+    use_polars=False,  # True gives TypeError
+    project=run_id
 )
-# n.b. we use the same regions twice
-path_to_regions = {
-    run_id: os.path.join(
-        f"{work_dir}/scATAC/consensus_peak_calling/consensus_regions.bed"
-    )
-}
-metadata_bc = pickle.load(
-    open(
-        os.path.join(f"{work_dir}/scATAC/quality_control/metadata_bc.pkl"),
-        "rb"
-    )
-)
-bc_passing_filters = pickle.load(
-    open(
-        os.path.join(
-            work_dir + "/scATAC/quality_control/bc_passing_filters.pkl"
-        ),
-        "rb"
-    )
-)
-cistopic_obj_list = [
-    create_cistopic_object_from_fragments(  # cistopic_class
-        path_to_fragments=fragments_dict[key],
-        path_to_regions=path_to_regions[key],
-        path_to_blacklist=black_lst,
-        metrics=metadata_bc[key],
-        valid_bc=bc_passing_filters[key],
-        n_cpu=77,
-        use_polars=False,  # True gives TypeError
-        project=key
-    )
-    for key in fragments_dict.keys()
-]
-
-cistopic_obj = merge(cistopic_obj_list)
 
 cistopic_obj.cell_data.to_csv(
     f"{work_dir}/cistopic_cell_data.csv", sep=",", header=True
 )
 
+# Create peaks.bed
 peak_file = pd.read_table(
-    str(
-        list(
-            pathlib.Path(
-                os.path.join(
-                    work_dir + "/scATAC/consensus_peak_calling/MACS/"
-                )
-            ).glob("*.narrowPeak")
-        )[0]
-    ),
+    glob.glob(
+        f"{work_dir}/scATAC/consensus_peak_calling/MACS/*.narrowPeak"
+    )[0],
     sep="\t",
     header=None
 )
@@ -421,7 +320,7 @@ peak_file.to_csv(
     f"{work_dir}/{run_id}_peaks.bed", sep="\t", header=False, index=False
 )
 
-print("peaks written to _peaks.bed")
+print("Peaks written to _peaks.bed.")
 
 print(
     r"""
